62444c4773c57045e868397fa217f03725afff74
Brett de Bear <bkdebear@gmail.com>
2023-01-08 14:33:10 -0800

    add script.js, add index.html

--- script.js
+++ script.js
@@ -1,4 +1,10 @@
 const h2 = document.createElement("h2");
-h2.textContent = "This content added by JavaScript";
+h2.textContent = "Random JavaScript Content";
 
 document.querySelector("body").appendChild(h2);
+
+const input = document.getElementById('button');
+function clickAlert() {
+    alert('OMG HIIII');
+}
+input.addEventListener('click', clickAlert);
